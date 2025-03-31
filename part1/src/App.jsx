9c0dc6e458965872a2d3b5b39ecf1c28ac486416
Hyunsang Cho <hyunsang4072@gmail.com>
2025-03-31 15:15:53 -0700

    finish part1 ex1.1 and ex1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// TODO: create Header, Content, and Total components
+const Header = (props) => {
+    return <h1>{props.course}</h1>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+const Part = (props) => {
+    return (
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+            {props.part} {props.exercise}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+};
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.part[0]} exercise={props.exercise[0]} />
+            <Part part={props.part[1]} exercise={props.exercise[1]} />
+            <Part part={props.part[2]} exercise={props.exercise[2]} />
+        </div>
+    );
+};
+
+function Total(props) {
+    return <p>{props.count}</p>;
 }
 
-export default App
+const App = () => {
+    const course = "Half Stack application development";
+    const parts = [
+        "Fundamentals of React",
+        "Using props to pass data",
+        "State of a component",
+    ];
+    const exercises = [10, 7, 14];
+    // const part1 = "Fundamentals of React";
+    // const exercises1 = 10;
+    // const part2 = "Using props to pass data";
+    // const exercises2 = 7;
+    // const part3 = "State of a component";
+    // const exercises3 = 14;
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content part={parts} exercise={exercises} />
+            <Total count={exercises[0] + exercises[1] + exercises[2]} />
+        </div>
+    );
+};
+
+export default App;
