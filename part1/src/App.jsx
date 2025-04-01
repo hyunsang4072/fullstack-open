ef8a70da24ccac8aae5882207b7ff5b4a3147034
Hyunsang Cho <hyunsang4072@gmail.com>
2025-03-31 18:03:05 -0700

    continue part1 event handling

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,52 +1,12 @@
-// TODO: create Header, Content, and Total components
-const Header = (props) => {
-    return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
-    return (
-        <p>
-            {props.part} {props.exercise}
-        </p>
-    );
-};
-
-const Content = (props) => {
-    return (
-        <div>
-            <Part part={props.part[0]} exercise={props.exercise[0]} />
-            <Part part={props.part[1]} exercise={props.exercise[1]} />
-            <Part part={props.part[2]} exercise={props.exercise[2]} />
-        </div>
-    );
-};
-
-function Total(props) {
-    return <p>{props.count}</p>;
-}
+import { useState } from "react";
 
 const App = () => {
-    const course = "Half Stack application development";
-    const parts = [
-        "Fundamentals of React",
-        "Using props to pass data",
-        "State of a component",
-    ];
-    const exercises = [10, 7, 14];
-    // const part1 = "Fundamentals of React";
-    // const exercises1 = 10;
-    // const part2 = "Using props to pass data";
-    // const exercises2 = 7;
-    // const part3 = "State of a component";
-    // const exercises3 = 14;
+    const [counter, setCounter] = useState(0);
 
-    return (
-        <div>
-            <Header course={course} />
-            <Content part={parts} exercise={exercises} />
-            <Total count={exercises[0] + exercises[1] + exercises[2]} />
-        </div>
-    );
+    setTimeout(() => setCounter(counter + 1), 1000);
+    console.log(counter);
+
+    return <div>{counter}</div>;
 };
 
 export default App;
