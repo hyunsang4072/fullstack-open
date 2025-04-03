4d6f4b188445d274425c28044fa216422b21b9c8
Hyunsang Cho <hyunsang4072@gmail.com>
2025-04-02 23:24:19 -0700

    learn event handling

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from "react";
 
+const Display = ({ count }) => {
+    return <div>{count}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+    return (
+        <div>
+            <button onClick={onClick}>{text}</button>
+        </div>
+    );
+};
+
 const App = () => {
     const [counter, setCounter] = useState(0);
 
-    setTimeout(() => setCounter(counter + 1), 1000);
-    console.log(counter);
+    const incrementByOne = () => setCounter(counter + 1);
+    const decrementByTwo = () => setCounter(counter - 2);
+    const resetCounter = () => setCounter(0);
 
-    return <div>{counter}</div>;
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <div>
+                <Display count={counter} />
+            </div>
+            <div>
+                <Button onClick={incrementByOne} text={"Add"} />
+            </div>
+            <div>
+                <Button onClick={decrementByTwo} text={"Subtract"} />
+            </div>
+            <div>
+                <Button onClick={resetCounter} text={"Reset"} />
+            </div>
+        </div>
+    );
 };
 
 export default App;
