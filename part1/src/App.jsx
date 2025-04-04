6ba2a0f5d4c673c7fb57f79302bbfbf764a8986e
Hyunsang Cho <hyunsang4072@gmail.com>
2025-04-04 16:58:12 -0700

    learn react states

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,37 +1,44 @@
 import { useState } from "react";
 
-const Display = ({ count }) => {
-    return <div>{count}</div>;
-};
-
-const Button = ({ onClick, text }) => {
-    return (
-        <div>
-            <button onClick={onClick}>{text}</button>
-        </div>
-    );
-};
-
 const App = () => {
-    const [counter, setCounter] = useState(0);
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [clicks, setClicks] = useState([]);
+    const [total, setAll] = useState(0);
 
-    const incrementByOne = () => setCounter(counter + 1);
-    const decrementByTwo = () => setCounter(counter - 2);
-    const resetCounter = () => setCounter(0);
+    const setLeftClick = () => {
+        setClicks([...clicks].concat("L"));
+        // console.log(clicks);
+        setLeft(left + 1);
+        setAll((t) => t + 1);
+    };
+
+    const setRightClick = () => {
+        setClicks([...clicks].concat("R"));
+        // console.log(clicks);
+        setRight(right + 1);
+        setAll((t) => t + 1);
+    };
+
+    console.log(total);
 
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                border: "2px solid",
+                padding: "50px 25px",
+                margin: "0 500px",
+            }}>
             <div>
-                <Display count={counter} />
-            </div>
-            <div>
-                <Button onClick={incrementByOne} text={"Add"} />
-            </div>
-            <div>
-                <Button onClick={decrementByTwo} text={"Subtract"} />
-            </div>
-            <div>
-                <Button onClick={resetCounter} text={"Reset"} />
+                {left}
+                <button onClick={setLeftClick}>left</button>
+                {total}
+                {clicks.join("")}
+                <button onClick={setRightClick}>right</button>
+                {right}
             </div>
         </div>
     );
