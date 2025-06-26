ef2288343b0bbaac901e0d3a883fe2d4a72035c1
Hyunsang Cho <hyunsang4072@gmail.com>
2025-06-26 01:42:48 -0700

    redeploy the app

--- part2/src/services/persons.js
+++ part2/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const baseUrl = "http://localhost:3001/api/persons";
-const baseUrl = "https://fullstack-open-lhoy.onrender.com/api/persons";
+// const baseUrl = "https://fullstack-open-lhoy.onrender.com/api/persons";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
     const request = axios.get(baseUrl);
